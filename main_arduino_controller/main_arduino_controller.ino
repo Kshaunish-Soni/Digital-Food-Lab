d3a1c74b0fd01f00a1cd0edb62691835b2ab6835
stonemao9 <stonemao9@gmail.com>
2017-06-22 15:47:32 -0700

    Loops

--- main_arduino_controller/main_arduino_controller.ino
+++ main_arduino_controller/main_arduino_controller.ino
@@ -1,18 +1,62 @@
+#include <Scheduler.h>
+
 #include <Time.h>
 #include <TimeLib.h>
 
 
 //Start Time -- needed for the light system and harvesting
+int sDay = 1;
+int sMonth = 1;
+int sYear = 17;
 
+//Current Time -- Will update every 30 sec
+int curDay = sDay;
+int curMonth = sMonth;
+int curHr = 0;
+int curMin = 0;
+int curSec = 0;
+
+//Target Time -- How much time has elapsed in light cycle
+int timeElapsed = 0;
+
+//Is it day or night
+
+
+//Different Pin #'s
+const int growthLight = 2;
 
 void setup() {
   Serial.begin(9600);
-  setTime(12,0,0,1,1,11);
+  setTime(0,0,0,sDay,sMonth,sYear);
+
+  // Light Setup
+  pinMode(growthLight, OUTPUT);
+
+  //Different Loop Schedules
+  Scheduler.startLoop(lightCycle);
+  Scheduler.startLoop(timeLoop);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(year());
-  delay(3000);
+  
+
+}
+
+//LIGHT FUNCTIONS
+void lightCycle(){
+  digitalWrite(growthLight, HIGH);
+  delay(5.76E7);    // 16 hours of light
+  digitalWrite(growthLight, LOW);
+  delay(7.2E6);     // 2 hours of dark 
+}
+
+//Saving time to raspberry pi / computer
+void timeLoop(){
+  curDay = day();
+  curMonth = month();
+  curHr = hour();
+  curMin = minute();
+  curSec = second();
+  delay(30000); //30sec dela
 }
 
