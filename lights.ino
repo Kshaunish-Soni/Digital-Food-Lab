1017402c4c42dc27c64df0d712587a664c69218e
anyali <anyamusical@gmail.com>
2017-06-22 13:06:25 -0700

    Update lights.ino

--- lights.ino
+++ lights.ino
@@ -1,12 +1,10 @@
 const int lights = #;
 // # = the digital pin number the lights are plugged in to
 void setup() {
-  // put your setup code here, to run once:
   pinMode(lights, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(lights, HIGH);
   delay(5.76E7);    // 16 hours of light
   digitalWrite(lights, LOW);
