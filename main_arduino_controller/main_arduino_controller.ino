4691b662a37534cefc4236231aba241f8ca51bf0
stonemao9 <stonemao9@gmail.com>
2017-06-24 12:31:15 -0700

    Added Raspberry pi to arduino integration

--- main_arduino_controller/main_arduino_controller.ino
+++ main_arduino_controller/main_arduino_controller.ino
@@ -6,16 +6,8 @@
 #include <Scheduler.h>
 
 //Start Time -- needed for the light system and harvesting
-int sDay = 1;
-int sMonth = 1;
-int sYear = 17;
-
-//Current Time -- Will update every 30 sec
-int curDay = sDay;
-int curMonth = sMonth;
-int curHr = 0;
-int curMin = 0;
-int curSec = 0;
+int dayElapsed = 0;
+int sYear = 2017;
 
 //Target Time -- How much time has elapsed in light cycle
 int timeElapsed = 0;
@@ -41,9 +33,29 @@ const int waterDepthSensor = 0;
 const int waterPump = 0;
 const int nutrientPump = 7;
 
+//Serial Data
+int serialCount = 0;
+
 void setup() {
   Serial.begin(9600);
-  setTime(0, 0, 0, sDay, sMonth, sYear);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  //Establish contact with Raspberry Pi
+  establishContact();
+  //Get time from Raspberry pi
+  if (Serial.available() > 0) {
+    int read1 = Serial.read();
+    int read2 = Serial.read();
+    int timeArray[4];
+    float myData = read1 * 10000;
+    for (int i = 0; i < 4; i++) {
+      timeArray[3 - i] = ((int)myData) % 100;
+      myData /= 100;
+    }
+    dayElapsed = read2;
+    setTime(timeArray[2], timeArray[3], 0, timeArray[1], timeArray[0], sYear);
+  }
 
   // Light Setup
   pinMode(growthLight, OUTPUT);
@@ -66,7 +78,7 @@ void setup() {
   pinMode(waterDepthSensor, INPUT);
   pinMode(waterPump, OUTPUT);
   pinMode(nutrientPump, OUTPUT);
-  
+
   // Allows us to run different loops simultaneously -- COMMENT OUT ONES YOU AREN'T GONNA TEST!
   Scheduler.startLoop(lightCycle);
   Scheduler.startLoop(timeLoop);
@@ -77,36 +89,40 @@ void setup() {
 
 // Main Debug Loop
 void loop() {
-  Serial.print("Virtual Time: "); Serial.print(hour());Serial.print(":");Serial.print(minute());Serial.print("\t");Serial.print(month());Serial.print("/");Serial.println(day());
-  Serial.print("Main Temperature (C): ");  Serial.println(getMainTemp());
-  Serial.print("am2315 Dual Sensor, Humidity: "); Serial.println(am2315.getHumidity());
-  Serial.print("am2315 Dual Sensor, Temperature in C: "); Serial.println(am2315.getTemperature_C());
-
-  //data array
-  Serial.print(getMainTemp());Serial.print(",");Serial.print(getTemp2());Serial.print(",");Serial.print(getBoxHumidity());Serial.print(",");Serial.print(getWaterDepth());
-  delay(5000);
+  if (Serial.available() > 0) {
+    Serial.write(getMainTemp());
+    Serial.write(getTemp2());
+    Serial.write(getBoxHumidity());
+    Serial.write(getWaterDepth());
+    Serial.write(dayElapsed);
+  }
+  delay(15000);
 }
 
 // LIGHT FUNCTIONS. Author: Anya Li. Editted By: Stone
 void lightCycle() {
-  digitalWrite(growthLight, HIGH);
-  isDay = true;
-  targetTemp = 65;
-  delay(5.76E7);    // 16 hours of light
-  digitalWrite(growthLight, LOW);
-  targetTemp = 55;
-  isDay = false;
-  delay(7.2E6);     // 2 hours of dark
+  /*
+   * After seed germinates (around 6 days), lights will turn on and follow cycle
+   * WILL ADD SALENIS DATA ON
+  */
+  if (dayElapsed > 6) {
+    digitalWrite(growthLight, HIGH);
+    isDay = true;
+    targetTemp = 65;
+    delay(5.76E7);    // 16 hours of light
+    digitalWrite(growthLight, LOW);
+    targetTemp = 55;
+    isDay = false;
+    delay(7.2E6);     // 2 hours of dark
+  } else {
+    digitalWrite(growthLight, LOW);
+  }
 }
 
 //Saving time to raspberry pi / computer
 void timeLoop() {
-  curDay = day();
-  curMonth = month();
-  curHr = hour();
-  curMin = minute();
-  curSec = second();
-  delay(30000); //30sec delay
+  delay(8.64e+7); // 24 Hrs
+  dayElapsed++;
 }
 
 // Controls the temperature. Author: Anya Li; Editted By: Stone
@@ -132,22 +148,22 @@ void tempControlLoop() {
 // Controls humidity of Farm. Author: Anya Li. Editted by: Stone Mao
 void humidityCheck() {
   int humidityValue = am2315.getHumidity();
-  if (humidityValue < 65.00){
+  if (humidityValue < 65.00) {
     digitalWrite(humidifier, HIGH);  // turn humidifier on if humidity is below 65%
   }
-  
+
   else if (humidityValue >= 67.00)
   {
     digitalWrite(humidifier, LOW);  // turn humidifier off if humidity is greater or equal to 67%
   }
-  
+
   delay(30000);   // check every 30 seconds
 }
 
 void hydroponicsLoop() {
   int level = 2;
   int waterDepth = getWaterDepth();
-  if(waterDepth>level-0){
+  if (waterDepth > level - 0) {
     digitalWrite(waterPump, LOW);
     digitalWrite(nutrientPump, LOW);
   } else {
@@ -155,6 +171,14 @@ void hydroponicsLoop() {
     digitalWrite(nutrientPump, HIGH);
   }
   delay(2000);
+}
+
+//Serial Connection handshake
+void establishContact() {
+  while (Serial.available() <= 0) { // If not established
+    Serial.print("A"); // Will send A
+    delay(300);
+  }
 }
 
 //Instance Methods -- For convenience
@@ -165,15 +189,15 @@ float getMainTemp() { // For main temperature sensor
   return (voltage - 0.5) * 100;
 }
 
-float getTemp2(){
+float getTemp2() {
   return am2315.getTemperature_C();
 }
 
-float getBoxHumidity(){
+float getBoxHumidity() {
   return am2315.getHumidity();
 }
 
-float getWaterDepth(){
+float getWaterDepth() {
   return analogRead(waterDepthSensor);
 }
 
--- main_controller.py
+++ main_controller.py
@@ -1,36 +1,71 @@
 import serial
 from datetime import datetime
 
+first_contact = False
+serial_data_Array = []
+serial_count = 0
 cereal_data = serial.Serial('/dev/ttyACM0',9600) #Connect to Arduino via USB on Serial 9600
+day = 0
 
 data_matrix = [[], #Time
                [], #Main Temp
                [], #Temp 2
                [], #Humidity
                [], #Water Depth
-               [], #pH
                ]
+def getTimeDecimal():
+    cur_time = datetime.now()
+    return cur_time.month*100+cur_time.day+cur_time.hour/100+cur_time.minute/10000
 
 try: #will try to add data to the output file
+    row = 0
     text_file = open("Output.txt", "r")
-    text_split = text_file.readline().split(",")
-    for x in text_split:
-        temp.append(int(x))
-    print(temp)
+
+    for line in range(1,5):
+        text_split = text_file.readline(line).split(",")
+        for num in text_split:
+            data_matrix[line].append(int(num))
+
+        print(data_matrix[line])
+        row += 1
+    day = text_file.readline(6)
+
 except FileNotFoundError:
     print("Output file not found, created one")
     text_file = open("Output.txt", "w")
     text_file.close()
+
 finally:
-    while 1:
-        arduinoData = cereal_data.readline()
-        splitData = arduinoData.spit(",")
-        #    Write code for GETTING ARRAY DATA
-        text_file = open("Output.txt", "w")
-        data_matrix[0].append(datetime.month*100+datetime.day)
-        text_file.write(",".join(str(e) for e in data_matrix[0]))
-        for row in range(len(data_matrix)-1):
-            data_matrix[row+1].append(int(splitData[row]));
-            text_file.write(",".join(str(e) for e in data_matrix[row]))
-        text_file.close()
-        print(data_matrix)
+    inByte = cereal_data.readline()
+    if not first_contact : #If connection isn't estb, estb it
+        if inByte == "A" :
+            first_contact = True
+            cereal_data.flush() #clear serial data
+            cur_time = datetime.now()
+            #writes time back to arduino
+            cereal_data.write(getTimeDecimal())
+    else:
+        while True:
+            serial_data_Array.append(inByte) #will store each byte into array
+            serial_count+=1 #Increment #
+            if(serial_count>4): #If there is five data bytes received
+                text_file = open("Output.txt", "w") #Opens data file
+
+                #Add current daytime to data matrix MonthMonthDayDay.HrHrMinMin
+                data_matrix[0].append(getTimeDecimal())
+
+                #Join current data into one string and save in file
+                text_file.write(",".join(str(e) for e in data_matrix[0]))
+
+                for row in range(len(data_matrix)-1): #doing row - 1 because date row has already been done
+                    data_matrix[row+1].append(int(serial_data_Array[row])) #adds in new data in row
+                    text_file.write(",".join(str(e) for e in data_matrix[row+1])) #adds it into the textfile
+
+                day = serial_data_Array[len(serial_data_Array)-1]
+                text_file.write(day) #Write the number of days
+
+                text_file.close()
+                print(data_matrix)
+
+                serial_count = 0 #Resets to get data
+                serial_data_Array = []
