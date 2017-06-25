fe4c177c0b2ad5cbeb419d7b4ff47b48234d97fe
stonemao9 <stonemao9@gmail.com>
2017-06-25 20:58:20 +0000

    It is storing data from arduino

--- main_controller.py
+++ main_controller.py
@@ -3,8 +3,7 @@ import time
 from datetime import datetime
 
 first_contact = False
-serial_data_Array = []
-serial_count = 0
+
 cereal_data = serial.Serial("/dev/ttyACM0",9600, timeout=1) #Connect to Arduino via USB on Serial 9600
 cereal_data.bytesize=serial.EIGHTBITS
 cereal_data.parity=serial.PARITY_NONE
@@ -12,7 +11,7 @@ cereal_data.stopbits=serial.STOPBITS_ONE
 cereal_data.xonxoff=False
 cereal_data.rtscts=False
 cereal_data.dsrdtr=False
-day = 0
+day = 3
 
 data_matrix = [[], #Time
                [], #Main Temp
@@ -28,21 +27,24 @@ try: #will try to add data to the output file
     row = 0
     text_file = open("Output.txt", "r")
 
-    for line in range(1,6):
-        text_split = text_file.readline(line).split(",")
+    for row in range(0,5):
+        text_split = text_file.readline(row).split(",")
         for num in text_split:
-            data_matrix[line].append(int(num))
+            print(text_split)
+            data_matrix[row].append(int(num))
 
         print(data_matrix[line])
-        row += 1
+        
     day = text_file.readline(6)
     print("Finish reading file")
 except IOError:
     print("Output file not found, created one")
-    #text_file = open("Output.txt", "w")
-    #text_file.close()
+    text_file = open("Output.txt", "w")
+    text_file.close()
 
 finally:
+    serial_data_Array = []
+    go = False
     inByte = cereal_data.read()
     if not first_contact : #If connection isn't estb, estb it
         print(inByte);
@@ -50,39 +52,37 @@ finally:
         cur_time = getTimeDecimal()
         #writes time back to arduino
         datadate = str(cur_time);
-        while(1):
+        while(not go):
             cereal_data.write(datadate+"\n"+str(day)+"\n") #Send the date time
             data = cereal_data.read()
-            if data != "A" and data !=" ":
-                while 1:
-                    cereal_data.write("Hi")
-                    print(cereal_data.readline())
-    else:
-        while True:
-            if(cereal_data.in_waiting > 0):
-                serial_data_Array.append(inByte) #will store each byte into array
-                serial_count+=1 #Increment #
-                if(serial_count>4): #If there is five data bytes received
+            if data != "A" and data !="":
+                time.sleep(2)
+                go = True
+                cereal_data.flushInput()
+                cereal_data.flushOutput()
+                cereal_data.flush()
+                break
+        while 1:
+            if go:
+                received_data = cereal_data.readline()
+
+                if received_data != "A" and received_data != "":
+                    num_array = [int(num) for num in received_data.split(".")] # Turns it into a string
                     text_file = open("Output.txt", "w") #Opens data file
 
-                    #Add current daytime to data matrix MonthMonthDayDay.HrHrMinMin
                     data_matrix[0].append(getTimeDecimal())
+                    text_file.write(",".join(str(e) for e in data_matrix[0])+"\n")
 
-                    #Join current data into one string and save in file
-                    text_file.write(",".join(str(e) for e in data_matrix[0]))
+                    for row in range(0,4):
+                        data_matrix[row+1].append(num_array[row])
+                        text_file.write(",".join(str(e) for e in data_matrix[row+1])+"\n") #adds it into the textfile
 
-                    for row in range(len(data_matrix)-1): #doing row - 1 because date row has already been done
-                        data_matrix[row+1].append(int(serial_data_Array[row])) #adds in new data in row
-                        text_file.write(",".join(str(e) for e in data_matrix[row+1])) #adds it into the textfile
-
-                    day = serial_data_Array[len(serial_data_Array)-1]
+                    day = num[len(num)-1]
                     text_file.write(day) #Write the number of days
 
                     text_file.close()
+                    
                     print(data_matrix)
-
-                    serial_count = 0 #Resets to get data
-                    serial_data_Array = []
-
-            cereal_data.write(day) # Will stimulate handshake
-            time.sleep(1);
+            
+                cereal_data.write(str(day)) # Will stimulate handshake
+        
