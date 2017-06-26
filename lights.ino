570bcd519a53c33533caa96b53a2e531beb80d02
anyali <anyamusical@gmail.com>
2017-06-26 15:11:20 -0700

    Update lights.ino

--- lights.ino
+++ lights.ino
@@ -1,12 +1,28 @@
-const int lights = #;
-// # = the digital pin number the lights are plugged in to
+int lights = 3;
+unsigned long previousMillis = 0;
+boolean isOn = true;
+long interval = 7.2E7;    // light interval is 20 hrs 7.2E7
 void setup() {
+  Serial.begin(9600);
   pinMode(lights, OUTPUT);
+  digitalWrite(lights, HIGH);
 }
 
 void loop() {
-  digitalWrite(lights, HIGH);
-  delay(5.76E7);    // 16 hours of light
-  digitalWrite(lights, LOW);
-  delay(7.2E6);     // 2 hours of dark 
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+    if (interval == 7.2E7) {
+      interval = 1.44E7;  // light interval is 4 hours 1.44E7
+      isOn = false;
+      digitalWrite(lights, LOW);
+    }
+    else {
+      interval = 7.2E7;
+      isOn = true;
+      digitalWrite(lights, HIGH);
+    }
+  }
 }
