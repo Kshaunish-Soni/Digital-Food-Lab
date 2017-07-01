b43d7eee57cbc2191858b01ee16c65dea972bb29
stonemao9 <stonemao9@gmail.com>
2017-07-01 21:13:12 +0000

    Add files via upload

--- food_controller.ino
+++ food_controller.ino
@@ -37,13 +37,26 @@ const int ultraEcho = 10;
 const int analogInPin = A1;
 float sensorValue = 0;
 
-const int waterOut = 0;
+const int waterIn = 0;
 const int phUp = 1;
 const int phDown = 2;
 const int nutrient = 9;
+const int waterOut = 7;
+
 long prevNutrientMillis = 0;
 boolean onceToday = false;
 boolean onceToday1 = false;
+
+////PH SETUP
+//#define SensorPin A0            //pH meter Analog output to Arduino Analog Input 0
+//#define Offset 0.00            //deviation compensate
+//
+//#define samplingInterval 20
+//#define printInterval 800
+//#define ArrayLenth  40    //times of collection
+//int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+//int pHArrayIndex=0;  
+//int timesChanged = 0;
 
 //Serial Data
 int serialCount = 0;
@@ -79,11 +92,7 @@ void setup() {
   pinMode(analogInPin, INPUT);
   pinMode(waterOut, OUTPUT);
   pinMode(phUp, OUTPUT);
-  pinMode(phDown, OUTPUT);
-  digitalWrite(waterOut, HIGH);
-  digitalWrite(phUp, LOW);
-  digitalWrite(phDown, LOW);
-}
+  pinMode(phDown, OUTPUT);}
 
 void loop() {
 
@@ -93,6 +102,8 @@ void loop() {
 //  hydroponicsLoop();
   humidityCheck();
   nutrientLoop();
+//  phLoop();
+  
 //HYDROPONICS LOOP
   float value = analogRead(A1);
 
@@ -195,38 +206,6 @@ void lightControlLoop() {
   }
 }
 
-//void hydroponicsLoop() {
-//  int level = 2;
-//  int waterDepth = analogRead(A1);
-//  Serial.println(waterDepth);
-//  if(waterDepth < 600){
-//    digitalWrite(waterOut, HIGH);
-//  } else {
-//       digitalWrite(waterOut, LOW);
-//  }
-//}
-
-//void hydroponicsLoop() {
-//  int level = 45.25;
-//  float waterDepth = getWaterDepth();
-//  Serial.println(waterDepth);
-//
-//   sensorValue = analogRead(analogInPin);
-//  //map it to the range of the analog out:
-//  //outputValue = map(sensorValue, 0, 1023, 0, 255);
-//  // change the analog out value:
-//  //analogWrite(analogOutPin, outputValue);
-//
-//  // print the results to the serial monitor:
-//  Serial.print("sensor = ");
-//  Serial.print(sensorValue / 10);
-//  
-//  if (sensorValue/10 <= 85) {
-//    digitalWrite(waterPump, HIGH);
-//  } else {
-//    digitalWrite(waterPump, LOW);
-//  }
-//}
 
 // Controls humidity of Farm. Author: Anya Li. Edited by: Stone Mao
 void humidityCheck() {
@@ -292,13 +271,74 @@ float getWaterDepthNew() {
   sensorValue = analogRead(analogInPin);
   return sensorValue;
 }
-//float getWaterDepth() {
-//  digitalWrite(ultraTrig,LOW);
-//  delayMicroseconds(2);
-//  digitalWrite(ultraTrig, HIGH);
-//  delayMicroseconds(10);
-//  digitalWrite(ultraTrig, LOW);
-//  int duration = pulseIn(ultraEcho, HIGH);
+
+//void phLoop() {
+//  static unsigned long samplingTime = millis();
+//  static unsigned long printTime = millis();
+//  static float pHValue,voltage;
+//  if(millis()-samplingTime > samplingInterval)
+//  {
+//      pHArray[pHArrayIndex++]=analogRead(SensorPin);
+//      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+//      voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+//      pHValue = 3.5*voltage+Offset;
+//      samplingTime=millis();
+//  }
+//  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+//  {
+//  Serial.print("Voltage:");
+//        Serial.print(voltage,2);
+//        Serial.print("    pH value: ");
+//  Serial.println(pHValue,2);
+//        printTime=millis();
+//  }
 //
-//  return 0.034*duration/2;
+////  if (pHValue > 6.5) {
+////    digitalWrite(, HIGH);
+////    
+////  }
+////  else  if {pHValue < 5) {
+////    digitalWrite(, HIGH);   
+////  }
 //}
+//
+//double avergearray(int* arr, int number){
+//  int i;
+//  int max,min;
+//  double avg;
+//  long amount=0;
+//  if(number<=0){
+//    Serial.println("Error number for the array to avraging!/n");
+//    return 0;
+//  }
+//  if(number<5){   //less than 5, calculated directly statistics
+//    for(i=0;i<number;i++){
+//      amount+=arr[i];
+//    }
+//    avg = amount/number;
+//    return avg;
+//  }else{
+//    if(arr[0]<arr[1]){
+//      min = arr[0];max=arr[1];
+//    }
+//    else{
+//      min=arr[1];max=arr[0];
+//    }
+//    for(i=2;i<number;i++){
+//      if(arr[i]<min){
+//        amount+=min;        //arr<min
+//        min=arr[i];
+//      }else {
+//        if(arr[i]>max){
+//          amount+=max;    //arr>max
+//          max=arr[i];
+//        }else{
+//          amount+=arr[i]; //min<=arr<=max
+//        }
+//      }//if
+//    }//for
+//    avg = (double)amount/(number-2);
+//  }//if
+//  return avg;  
+//}
+
